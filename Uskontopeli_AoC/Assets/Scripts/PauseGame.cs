b8d126f53870b9325790f32d497504d76f1a35b0
Jaakko <jaakko@kronogra.fi>
2017-02-18 00:26:54 +0200

    added features
    addeddeed pause menu and locked the rusor into the screen and exe

---
+++ Uskontopeli_AoC/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+    public Transform canvas;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void Update ()
+    {
+        if(canvas.gameObject.activeInHierarchy == false)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+                Pause();
+        }
+	}
+    
+    public void Pause()
+    {
+        if (canvas.gameObject.activeInHierarchy == false)
+        {
+            canvas.gameObject.SetActive(true);
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            canvas.gameObject.SetActive(false);
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Exited");
+    }
+
+}
+ 
