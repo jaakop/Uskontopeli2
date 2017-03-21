417a03016038695ab30391e546da9961b2617ca3
Jaakko <jaakko@kronogra.fi>
2017-03-21 19:53:09 +0200

    Shit hapens

---
+++ Uskontopeli_AoC/Assets/Scripts/ChaserScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaserScript : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject Player;
+
+	void Start () {
+		
+	}
+
+
+	void Update () {
+        transform.rotation = Player.transform.rotation;
+	}
+}
